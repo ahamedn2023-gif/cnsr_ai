b609793e600cf82916f19a17d2e744e926ca13b7
mohammed20010905 <mohammed20010905@gmail.com>
2026-09-06 18:11:57 +0530

    Fix Render deployment

--- app.py
+++ app.py
@@ -236,4 +236,5 @@ def logout():
 # Run Flask
 # -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
